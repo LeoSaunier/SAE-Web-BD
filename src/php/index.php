3e64ca7a094c09e587b9aa5c505849aefa66b6f2
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2025-01-06 17:53:02 +0100

    Page accueil sans css

---
+++ src/php/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poney Club Grand Galop</title>
+</head>
+
+<body>
+    <header>
+        <h1>Pony Club Grand Galop</h1>
+        <img src="img/logo_ffe.png" alt="logo_ffe">
+        <a href="">Se connecter</a>
+        <li>
+            <ul><a href="#a_propos">À propos</a></ul>
+            <ul><a href="#services">Nos services</a></ul>
+            <ul><a href="">Horaires</a></ul>
+            <ul><a href="">Contacts</a></ul>
+        </li>
+    </header>
+    <article>
+        <img src="img/baniere.png" alt="baniere">
+        <section id="a_propos">
+            <h2>À propos de nous</h2>
+            <p>Situé en Sologne, le poney-club Grand Galop accueille les cavaliers selon leur niveau (débutant ou
+                confirmé) pour pratiquer l'équitation dans des cours adaptés et sécurisé avec leur animal favori, parmi
+                nos 30 poney disponibles.Nous proposons des cours particuliers et collectifs, avec une cotisation
+                annuelle et un système de réservation en ligne simple et efficace.</p>
+        </section>
+        <section id="services">
+            <h2>Nos services</h2>
+            <section class="service">
+                <h3>Cours collectifs</h3>
+                <p>Jusqu’à 10 personnes, participez à ces cours pour une ambiances conviviale.</p>
+            </section>
+            <section class="service">
+                <h3>Cours particuliers</h3>
+                <p>Des scéances d’équitations sur mesure pour une progression rapide.</p>
+            </section>
+        </section>
+        <a href="">Voir les tarifs</a>
+    </article>
+    <footer></footer>
+</body>
+
+</html>
