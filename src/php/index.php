ec76aa336c1a3ef57352f9cde06f7209362c1403
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2025-01-10 16:46:18 +0100

    Ajout header.php

--- src/php/index.php
+++ src/php/index.php
@@ -9,23 +9,11 @@
 </head>
 
 <body>
-    <header>
-        <div id="img_container">
-            <img src="img/logo_ffe.png" alt="logo_ffe">
-        </div>
-        <div id="center">
-            <h1>Poney Club Grand Galop</h1>
-            <ul id="options">
-                <li><a href="#a_propos">À propos</a></li>
-                <li><a href="#services">Nos services</a></li>
-                <li><a href="">Horaires</a></li>
-                <li><a href="">Contacts</a></li>
-            </ul>
-        </div>
-        <a href="" id="connect">Se connecter</a>
-    </header>
+    <?php
+    require "header.php";
+    ?>
     <article>
-        <img src="img/baniere.png" alt="baniere" width="100%">
+        <img src="img/baniere.png" alt="baniere">
         <section id="a_propos">
             <h2>À propos de nous</h2>
             <p>Situé en Sologne, le poney-club Grand Galop accueille les cavaliers selon leur niveau (débutant ou
