4e03818d9e21cb1f115c47deedef6a53d3569210
RyryCantonais <ryrycantonais@gmail.com>
2025-01-12 01:12:54 +0100

    Ajout script date.php et de page tarfis et reservations etu et prof

---
+++ src/php/reservationsProf.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <title>Poney Club Grand Galop</title>
+</head>
+
+<body>
+    <?php
+    require "header.php";
+    ?>
+    <div id="reservation">
+        <h2>Réservations</h2>
+        <label for="yearL">Année</label>
+        <select name="yearS" id="yearS">
+            <option value="">PlaceHolder</option>
+            <option value="">PlaceHolder</option>
+        </select>
+        <label for="MonthL">Mois</label>
+        <select name="MonthS" id="MonthS">
+            <option value="1">Janvier</option>
+            <option value="2">Février</option>
+            <option value="3">Mars</option>
+            <option value="4">Avril</option>
+            <option value="5">Mai</option>
+            <option value="6">Juin</option>
+            <option value="7">Juillet</option>
+            <option value="8">Août</option>
+            <option value="9">Septembre</option>
+            <option value="10">Octobre</option>
+            <option value="11">Novembre</option>
+            <option value="12">Décembre</option>
+        </select>
+        <label for="DaysL">Jours</label>
+        <select name="DaysS" id="DaysS">
+            <option value="1">Janvier</option>
+            <option value="2">Février</option>
+            <option value="3">Mars</option>
+            <option value="4">Avril</option>
+            <option value="5">Mai</option>
+            <option value="6">Juin</option>
+            <option value="7">Juillet</option>
+            <option value="8">Août</option>
+            <option value="9">Septembre</option>
+            <option value="10">Octobre</option>
+            <option value="11">Novembre</option>
+            <option value="12">Décembre</option>
+        </select>
+    </div>
+</body>
+
+</html>
